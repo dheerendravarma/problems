d3549a10b29e14a47ab94f0bb5ad2c230ea65944
dheerendravarma <dheerendra557@gmail.com>
2024-03-05 11:00:47 +0530

    Showing results for all test cases

--- trees/btree_height.py
+++ trees/btree_height.py
@@ -1,5 +1,28 @@
+from trees import TreeNode, buid_tree
+
+"""
+Test Cases
+3
+1 2 3 N N N N
+2 N 1 N N 3 N N N N N N N
+1 2 N 3 N N N 4 N N N N N N N N N
+"""
+def get_height(root: TreeNode) -> int:
+    if root is None:
+        return 0
+    return max(get_height(root.left), get_height(root.right)) + 1
+
+
 def main():
-    pass
+    test_cases: int = int(input())
+    results = []
+    for _ in range(test_cases):
+        tree_str: str = input()
+        root: TreeNode = buid_tree(tree_str)
+        height: int = get_height(root)
+        results.append(height)
+    for res in results:
+        print(res)
 
 if __name__ == "__main__":
     main()
--- trees/in_order.py
+++ trees/in_order.py
@@ -3,8 +3,9 @@ from typing import List
 
 """
 Test Cases
-1
+2
 1 2 3 N N N N
+1 2 N 3 N N N 4 N N N N N N N N N
 """
 def in_order(root: TreeNode) -> List:
     if root is None:
@@ -14,11 +15,14 @@ def in_order(root: TreeNode) -> List:
 
 def main():
     test_cases: int = int(input())
+    results = []
     for _ in range(test_cases):
         tree_str: str = input()
         root: TreeNode = buid_tree(tree_str)
         inorder_list: List = in_order(root)
-        print(" ".join(inorder_list))
+        results.append(inorder_list)
+    for res in results:
+        print(" ".join(res))
 
 if __name__ == "__main__":
     main()

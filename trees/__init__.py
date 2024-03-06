508feadcb06928f3be0ffbdba5f4c6500245797d
dheerendravarma <dheerendra557@gmail.com>
2024-03-06 11:18:14 +0530

    Fixed build_tree method

--- trees/__init__.py
+++ trees/__init__.py
@@ -9,40 +9,18 @@ class TreeNode:
 
 def buid_tree(tree_str: str) -> Optional[TreeNode]:
     # Return Empty Tree If no nodes found in the String
-    nodes_length = len(tree_str)
-    if nodes_length == 0 or tree_str[0] == "N":
+    if len(tree_str) == 0 or tree_str[0] == "N":
         return
     
     input_nodes = list(map(str, tree_str.split()))
+    nodes_length = len(input_nodes)
 
-    # Create the QUEUE
-    queue_length = 0
-    queue = list()
+    def build_node(index):
+        if index < nodes_length and input_nodes[index] != 'N':
+            root = TreeNode(input_nodes[index])
+            root.left = build_node(2 * index + 1)
+            root.right = build_node(2 * index + 2)
+            return root
+        return None
 
-    # create the root node
-    root_node = TreeNode(input_nodes[0])
-    queue.append(root_node)
-    queue_length += 1
-
-    idx = 1
-    while(idx < nodes_length and queue_length > 0):
-        node = queue.pop(0)
-        queue_length -= 1
-
-        left_value = input_nodes[idx]
-        if left_value != "N":
-            node.left = TreeNode(left_value)
-            queue.append(node.left)
-            queue_length += 1
-        
-        idx += 1
-        if(idx >= nodes_length):
-            break
-
-        right_value = input_nodes[idx]
-        if right_value != "N":
-            node.right = TreeNode(right_value)
-            queue.append(node.right)
-            queue_length += 1
-        idx += 1
-    return root_node
+    return build_node(0)

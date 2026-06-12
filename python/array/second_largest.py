d67e31aca6d00c5a6a8231e53de7c888f0e4799b
Dheerendra Varma <dheerendra557@gmail.com>
2026-06-12 19:41:40 +0530

    Fixed the pre-commit issues

--- python/array/second_largest.py
+++ python/array/second_largest.py
@@ -1,8 +1,11 @@
 def second_largest_naive(numbers):
     pass
 
+
 def main():
     numbers = list(map(int, input().split(" ")))
+    second_largest_naive(numbers)
+
 
 if __name__ == "__main__":
     main()
